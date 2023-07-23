3fa579328cc697b7399ffdf9597217bbd2cfbd15
Adam Kuffel <adam@kuffel.eu>
2023-07-23 20:30:00 +0200

    Added figure icons

--- lib/chess/figure.ex
+++ lib/chess/figure.ex
@@ -1,8 +1,9 @@
 defmodule Chess.Figure do
   @moduledoc """
-  Figure module
+  Defines a struct and functions for working with figures.
   """
 
+  @type t() :: %__MODULE__{color: String.t(), type: String.t()}
   defstruct color: "", type: ""
 
   alias Chess.Figure
@@ -24,4 +25,28 @@ defmodule Chess.Figure do
   def new(color, type)
       when color in ["w", "b"] and type in ["p", "r", "n", "b", "q", "k"],
       do: %Figure{color: color, type: type}
+
+  @doc """
+  Returns the figures unicode icon
+
+  ## Examples
+
+      iex> Chess.Figure.icon(%Chess.Figure{type: "p", color: "w"})
+      "♙"
+
+  """
+  @spec icon(figure :: Figure.t()) :: String.t()
+  def icon(%__MODULE__{type: "p", color: "w"}), do: "\u2659"
+  def icon(%__MODULE__{type: "p", color: "b"}), do: "\u265F"
+  def icon(%__MODULE__{type: "r", color: "w"}), do: "\u2656"
+  def icon(%__MODULE__{type: "r", color: "b"}), do: "\u265C"
+  def icon(%__MODULE__{type: "k", color: "w"}), do: "\u2654"
+  def icon(%__MODULE__{type: "k", color: "b"}), do: "\u265A"
+  def icon(%__MODULE__{type: "q", color: "w"}), do: "\u2655"
+  def icon(%__MODULE__{type: "q", color: "b"}), do: "\u265B"
+  def icon(%__MODULE__{type: "b", color: "w"}), do: "\u2658"
+  def icon(%__MODULE__{type: "b", color: "b"}), do: "\u265E"
+  def icon(%__MODULE__{type: "n", color: "w"}), do: "\u2657"
+  def icon(%__MODULE__{type: "n", color: "b"}), do: "\u265D"
+  # def icon(nil), do: " "
 end
--- test/chess/figure_test.exs
+++ test/chess/figure_test.exs
@@ -1,12 +1,41 @@
 defmodule Chess.FigureTest do
   use ExUnit.Case
-
+  doctest Chess.Figure
   alias Chess.Figure
 
-  test "create figure" do
-    %Figure{color: color, type: type} = Figure.new("w", "p")
+  figures = [
+    {"white pawn", "w", "p", "♙"},
+    {"black pawn", "b", "p", "♟"},
+    {"white rook", "w", "r", "♖"},
+    {"black rook", "b", "r", "♜"},
+    {"white knight", "w", "n", "♗"},
+    {"black knight", "b", "n", "♝"},
+    {"white bishop", "w", "b", "♘"},
+    {"black bishop", "b", "b", "♞"},
+    {"white queen", "w", "q", "♕"},
+    {"black queen", "b", "q", "♛"},
+    {"white king", "w", "k", "♔"},
+    {"black king", "b", "k", "♚"}
+  ]
 
-    assert color == "w"
-    assert type == "p"
+  test "new/2 should raise an error when color and type are not supported" do
+    assert_raise FunctionClauseError, ~r/^no function clause matching/, fn ->
+      Figure.new("x", "y")
+    end
+  end
+
+  for {name, color, type, _} <- figures do
+    test "new/2 should support #{name}" do
+      assert Figure.new(unquote(color), unquote(type)) == %Figure{
+               color: unquote(color),
+               type: unquote(type)
+             }
+    end
+  end
+
+  for {name, color, type, icon} <- figures do
+    test "icon/1 should support #{name}" do
+      assert Figure.icon(%Figure{color: unquote(color), type: unquote(type)}) == unquote(icon)
+    end
   end
 end
